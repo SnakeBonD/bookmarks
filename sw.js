0251bd97e0bf336401713e9a3824657e63a3418a
SnakeBonD <SnakeBonD@users.noreply.github.com>
2026-09-21 19:04:02 +0200

    chore: refresh PWA cache for v0.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bookmarks-v0.8-shell';
+const CACHE='bookmarks-v0.9-shell';
 const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/favicon.svg'];
 
 self.addEventListener('install',event=>{
